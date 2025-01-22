fbb9b1e5a434aa802d8bf48fa904d7af3ea58320
devjs2705 <devjs2705@gmail.com>
2025-01-22 15:24:27 +0530

    created route to get user data from DB and JWT verification middleware done

--- server/routes/auth-route.js
+++ server/routes/auth-route.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const authControllers = require("../controllers/auth-controller.js")
 const validate = require("../middlewares/validate-middleware.js")
 const {signUpSchema, loginSchema} = require("../validators/auth-validator.js")
+const authMiddleware = require("../middlewares/auth-middleware.js")
 
 // Home page
 router.route("/").get(authControllers.home);
@@ -12,5 +13,8 @@ router.route("/register").post(validate(signUpSchema) , authControllers.register
 
 // Login / Sign-In
 router.route("/login").post(validate(loginSchema), authControllers.login);
+
+// User / to get user data
+router.route("/user").get(authMiddleware, authControllers.user);
 
 module.exports = router;
